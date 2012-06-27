06c6ad4c610273f0fdef240af5a9a95c0926a105
Niels van de Weem <NielsGithub@vdweem.com>
2012-06-27 17:43:38 +0200

    Initialize Jah'Spotify with a temp folder

---
+++ src/main/java/com/vdweem/webspotify/SpotifyInitializer.java
@@ -0,0 +1,29 @@
+package com.vdweem.webspotify;
+
+import jahspotify.services.JahSpotifyService;
+
+import java.io.File;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+/**
+ * Initializes Jah'Spotify with a setting folder.
+ * @author Niels
+ *
+ */
+public class SpotifyInitializer implements ServletContextListener{
+
+	@Override
+	public void contextInitialized(ServletContextEvent sce) {
+		File root = new File(sce.getServletContext().getRealPath(".")).getParentFile().getParentFile();
+		File config = new File(root, "jahspotify");
+		config.mkdirs();
+		JahSpotifyService.initialize(config);
+	}
+
+	@Override
+	public void contextDestroyed(ServletContextEvent sce) {
+	}
+
+}
